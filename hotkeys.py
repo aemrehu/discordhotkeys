ef5b92a01ebbd82804282bad80a497a8ed879518
aemrehu <aemrehu@gmail.com>
2023-06-08 19:27:02 +0300

    improvements and formatting

--- hotkeys.py
+++ hotkeys.py
@@ -3,61 +3,82 @@ import requests
 import tomli
 from pynput import keyboard
 
-TIMEOUT = 10
-URL = ""
-header = {}
+class Hotkeys:
 
-resume = {
-    "content": "m!resume"
-}
-pause = {
-    "content": "m!pause"
-}
-skip = {
-    "content": "m!s"
-}
-summon = {
-    "content": "m!summon"
-}
-foff = {
-    "content": "m!fuck off"
-}
+    TIMEOUT = 10
+    url = ""
+    paused: bool = False
+    header = {}
 
-def on_activate_pause():
-    requests.post(URL, json=pause, headers=header, timeout=TIMEOUT)
-    print("PAUSE")
+    resume  = {"content": "m!resume"}
+    pause   = {"content": "m!pause"}
+    skip    = {"content": "m!s"}
+    summon  = {"content": "m!summon"}
+    foff    = {"content": "m!fuck off"}
 
-def on_activate_resume():
-    requests.post(URL, json=resume, headers=header, timeout=TIMEOUT)
-    print("RESUME")
+    def __init__(self, arg) -> None:
+        self.read_file(arg)
 
-def on_activate_skip():
-    requests.post(URL, json=skip, headers=header, timeout=TIMEOUT)
-    print("SKIP")
+    def on_activate_pauseresume(self):
+        if self.paused:
+            self.paused = False
+            requests.post(self.url, json=self.resume, headers=self.header, timeout=self.TIMEOUT)
+            print("RESUME")
+        else:
+            self.paused = True
+            requests.post(self.url, json=self.pause, headers=self.header, timeout=self.TIMEOUT)
+            print("PAUSE")
 
-def on_activate_summon():
-    requests.post(URL, json=summon, headers=header, timeout=TIMEOUT)
-    print("SUMMON")
+    # def on_activate_pause(self):
+    #     requests.post(self.url, json=self.pause, headers=self.header, timeout=self.TIMEOUT)
+    #     print("PAUSE")
 
-def on_activate_fuckoff():
-    requests.post(URL, json=foff, headers=header, timeout=TIMEOUT)
-    print("FUCKOFF")
+    # def on_activate_resume(self):
+    #     requests.post(self.url, json=self.resume, headers=self.header, timeout=self.TIMEOUT)
+    #     print("RESUME")
+
+    def on_activate_skip(self):
+        requests.post(self.url, json=self.skip, headers=self.header, timeout=self.TIMEOUT)
+        print("SKIP")
+
+    def on_activate_summon(self):
+        requests.post(self.url, json=self.summon, headers=self.header, timeout=self.TIMEOUT)
+        print("SUMMON")
+
+    def on_activate_fuckoff(self):
+        requests.post(self.url, json=self.foff, headers=self.header, timeout=self.TIMEOUT)
+        print("FUCKOFF")
+
+    def on_activate_terminate(self):
+        sys.exit()
+
+    def read_file(self, arg):
+        try:
+            with open(arg, "rb") as file:
+                settings = tomli.load(file)
+            for key, value in settings["header"].items():
+                self.header[key] = value
+            for _, value in settings["url"].items():
+                self.url = value
+        except Exception:
+            raise Exception("Check .toml file.")
+
+    def run(self):
+        print("Hotkeys:")
+        print("    ctrl + ä = pause and resume")
+        print("    ctrl + ö = skip")
+        print("    ctrl + l = summon")
+        print("    ctrl + k = kick off")
+        print("    ctrl + alt + q = terminate hotkeys")
+        with keyboard.GlobalHotKeys({
+            '<ctrl>+ä': self.on_activate_pauseresume,
+            '<ctrl>+ö': self.on_activate_skip,
+            '<ctrl>+l': self.on_activate_summon,
+            '<ctrl>+k': self.on_activate_fuckoff,
+            '<ctrl>+<alt>+q': self.on_activate_terminate
+        }) as h: h.join()
+
 
 if __name__ == "__main__":
-    try:
-        with open(sys.argv[1], "rb") as file:
-            settings = tomli.load(file)
-        for key, value in settings["header"].items():
-            header[key] = value
-        for _, value in settings["url"].items():
-            URL = value
-    except Exception:
-        raise Exception("Please provide proper settings.toml file.")
-
-    with keyboard.GlobalHotKeys({
-        '<alt_gr>+<shift>+p': on_activate_pause,
-        '<alt_gr>+<shift>+o': on_activate_resume,
-        '<alt_gr>+<shift>+i': on_activate_skip,
-        '<alt_gr>+<shift>+å': on_activate_summon,
-        '<alt_gr>+<shift>+q': on_activate_fuckoff
-    }) as h: h.join()
+    hotkeys = Hotkeys(sys.argv[1])
+    hotkeys.run()
